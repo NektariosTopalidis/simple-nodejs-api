ac9df0ac7775e0b60c30592e49946eb6ece07ae6
Κωνσταντινος Αδαμουδας <90853664+Javakios@users.noreply.github.com>
2023-05-03 12:31:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,27 @@ const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
-
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use('/users', usersRoutes);
 
 app.get('/', (req,res) => {
     res.send('Hello from homepage.');
 })
-
+app.use((error, req, res, next) => {
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
 app.listen(PORT, () => {
     console.log(`Server running on port http://localhost:${PORT}`);
 })
